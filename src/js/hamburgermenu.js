97305a175cfbdb360fd72815817305d25abcdd8a
Emma Forslund <emfo2102@student.miun.se>
2022-06-05 19:34:13 +0200

    Ändrat namn på hamburgermeny-funktionen och bytt plats på variablerna

--- src/js/hamburgermenu.js
+++ src/js/hamburgermenu.js
@@ -1,22 +1,13 @@
 /*
  * @Author: Emma Forslund - emfo2102 
  * @Date: 2022-06-02 00:47:50 
- * @Last Modified by:   Emma Forslund - emfo2102 
- * @Last Modified time: 2022-06-02 00:47:50 
+ * @Last Modified by: Emma Forslund - emfo2102
+ * @Last Modified time: 2022-06-04 18:02:01
  */
 
-
-//Variabler
-let hamburger = document.getElementById("hamburger-icon");
-let navUl = document.getElementById("nav-ul");
-
-window.onload = init;
-
-function init() {
-
-    //Funktion för hamburger-menyn
-    hamburger.addEventListener("click", () => {
-        navUl.classList.toggle("show");
-    })
-
+function hamburgerMenu(){
+  //Funktion för hamburger-menyn
+  hamburger.addEventListener("click", () => {
+    navUl.classList.toggle("show");
+  })
 }
